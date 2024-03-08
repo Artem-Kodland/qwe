14cab0333323ff10b9bdd0f0020ee30240312886
Artem-Kodland <134316152+Artem-Kodland@users.noreply.github.com>
2024-03-08 21:25:02 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from model import get_class
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+       for attachment in ctx.message.attachments:
+           file_name = attachment.filename
+           file_url = attachment.url
+           await attachment.save(f'images/{file_name}')
+           await ctx.send(get_class(model="keras_model.h5", labels="labels.txt", image=f"images/{file_name}"))
+    else:
+        await ctx.send('Вы забыли загрузить картинку')
+        
+bot.run("OTM1ODQ0MTYzNzk3MjYyMzM4.GkfsXo.DhnxJ4SM6v68cWOs039iilRFLRL2BcNfSkODnQ")
